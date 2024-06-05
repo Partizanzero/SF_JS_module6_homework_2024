14a8aff0022bbd8769ff78c93449808ba4bed778
Partizanzero <ElenaD81@list.ru>
2024-06-05 23:38:33 +0400

    Update task_6.6.3.js

--- task_6.6.3.js
+++ task_6.6.3.js
@@ -1,7 +1,13 @@
 let anyNum = prompt("Введите любое число");
-+anyNum;
 
-let arr = []; 
+if (isNaN(anyNum)) {
+  alert("Вы ввели не число. Попробуйте еще раз");
+} else {
+  alert("Вы ввели число");
+  +anyNum;
+}
+
+let arr = [];
 
 for (i = 0; i <= anyNum; i++) {
   arr.push(i);
