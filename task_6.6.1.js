49624dce5b482027a46b5b976f6ea4eb8e30ccf0
Partizanzero <ElenaD81@list.ru>
2024-06-09 15:46:47 +0400

    Update task_6.6.1.js

--- task_6.6.1.js
+++ task_6.6.1.js
@@ -1,31 +1,53 @@
-function isPal(str) {
-  let newstr; //str с убранными пробелами
-  let newstrarr; //массив букв из строки
-  let palidr; //палиндром
+//Mentor edits:
 
+/* 
+1) классно, что решили проверить на тип, но тогда уже лучше вот так ( if (typeof str === "string") {
+const  newstr = str.toLowerCase(); //приводим все к прописным буквам} else {
+return 'Введите строку'
+})
+
+Статус: Правки внесены
+
+2) нейминг переменных в JS - camelCase. Не стоит сокращать имена констант, важно писать читаемый код.
+
+Статус: Правки внесены
+
+3) строка в JS уникальна нет смысла объявлять ее через let
+
+Статус: Правки внесены (комментарий ученика: прикольно! )) ) 
+
+4) так же можно сократить количество переменных в вашей задаче с помощью точечной записи 
+' newstr = str.toLowerCase().replace(/\s+/g, ""). ' если так соединим то пробелов уже не будет. 
+
+Статус: Правки внесены
+
+5) Второй метод лишний  palidr = newstrarr.join("").replace(/\s+/g, ""); 
+
+Статус: Правки внесены
+*/
+
+
+
+function isPalindrom(originalString) {
   //проверяем, что передана строка
-  if (typeof str === "string") {
-    newstr = str.toLowerCase(); //приводим все к прописным буквам
-  } else {
-    newstr = str.toString.toLowerCase(); //приводим в строковому формату
+  if (typeof originalString !== "string") {
+    return "Введите строку";
   }
+  
+  newString = originalString.replace(/\s+/g, ""); //убираем все пробелы в строке: \s+ - учитывать более одного пробела подряд, g - глобальный флаг - ищет пробелы по всей строке
 
+  newStringArray = newString.split(""); //режем строку на массив букв
+  palindrom = newStringArray.reverse().join(""); //переворачиваем массив букв
  
-  newstr = newstr.replace(/\s+/g, "");  //убираем все пробелы в строке: \s+ - учитывать более одного пробела подряд, g - глобальный флаг - ищет пробелы по всей строке
-
-  newstrarr = newstr.split(""); //режем строку на массив букв
-  newstrarr.reverse(); //переворачиваем массив букв
-
-  palidr = newstrarr.join("").replace(/\s+/g, ""); //соединяем перевернутый массив букв в единую строку, убрав все пробелы
-
   //проверяем является ли слово палиндромом
-  if (palidr === newstr) {
-    console.log(`Слово ${str} является палиндромом`);
+  if (palindrom.toLowerCase() === newString.toLowerCase()) {
+    console.log(`Слово ${originalString} является палиндромом`);
   } else {
-    console.log(`Слово ${str} не является палиндромом`);
+    console.log(`Слово ${originalString} не является палиндромом`);
   }
 }
 
-isPal("Довод");
-isPal("Сантимент");
-isPal("А роза упала на лапу Азора");
+//Test
+isPalindrom("Довод");
+isPalindrom("Сантимент");
+isPalindrom("А роза упала на лапу Азора");
